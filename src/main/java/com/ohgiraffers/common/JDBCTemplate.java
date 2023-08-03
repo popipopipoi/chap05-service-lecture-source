a68487d2bb02f8e1ac19937806f584799cce1a13
blimu <sheep.ohgiraffers@gmail.com>
2023-08-04 08:54:07 +0900

    commit

---
+++ src/main/java/com/ohgiraffers/common/JDBCTemplate.java
@@ -0,0 +1,97 @@
+package com.ohgiraffers.common;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCTemplate {
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        Properties prop = new Properties();
+
+        try {
+            prop.load(new FileReader("src/main/java/com/ohgiraffers/config/connection-info.properties"));
+
+            String driver = prop.getProperty("driver");
+            String url = prop.getProperty("url");
+
+            Class.forName(driver);
+
+            con = DriverManager.getConnection(url, prop);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+
+    public static void close(Connection con) {
+
+        try {
+            if(con != null & !con.isClosed()) {
+                con.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void close(Statement stmt) {
+
+        try {
+            if(stmt != null && !stmt.isClosed()) {
+                stmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void close(ResultSet rset) {
+
+        try {
+            if(rset != null && !rset.isClosed()) {
+                rset.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* 설명. 수동 commit 해본 후 추가할 내용 */
+    public static void commit(Connection con) {
+        try {
+            if(con != null && !con.isClosed()) {
+                con.commit();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void rollback(Connection con) {
+        try {
+            if(con != null && !con.isClosed()) {
+                con.rollback();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
